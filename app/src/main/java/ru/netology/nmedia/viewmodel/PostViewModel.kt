842d3b3a52447fc35430ad8d333ab1fe1c32c2e7
dmitry-mak <akkaru@gmail.com>
2026-02-11 19:29:10 +0200

    Lesson 10 homework- In file data storage. - base solution

--- app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -1,10 +1,14 @@
 package ru.netology.nmedia.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.repository.PostRepository
+import ru.netology.nmedia.repository.PostRepositoryFilesImpl
 import ru.netology.nmedia.repository.PostRepositoryInMemory
+import ru.netology.nmedia.repository.PostRepositorySharedPrefsImpl
 
 private val empty = Post(
     id = 0,
@@ -16,9 +20,9 @@ private val empty = Post(
     isLiked = false
 )
 
-class PostViewModel : ViewModel() {
+class PostViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val repository: PostRepository = PostRepositoryInMemory()
+    private val repository: PostRepository = PostRepositoryFilesImpl(application)
 
     val data = repository.getData()
     val edited = MutableLiveData(empty)
@@ -44,11 +48,11 @@ class PostViewModel : ViewModel() {
         edited.value = empty
     }
 
-    fun edit(post: Post){
+    fun edit(post: Post) {
         edited.value = post
     }
 
-    fun cancelEditing(){
+    fun cancelEditing() {
         edited.value = empty
-        }
+    }
 }
