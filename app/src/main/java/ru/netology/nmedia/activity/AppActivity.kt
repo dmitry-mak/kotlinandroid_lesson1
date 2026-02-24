383d39168ed4cf06a077f69eeaaca299367ddcbf
dmitry-mak <akkaru@gmail.com>
2026-02-24 10:29:28 +0200

    Lesson 14- Push notification from server. Added try-catch for IllegalArgumentException to catch unknown notification type

--- app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
+++ app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
@@ -4,12 +4,10 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.firebase.messaging.FirebaseMessaging
 import ru.netology.nmedia.databinding.ActivityAppBinding
 
 class AppActivity : AppCompatActivity() {
--- app/src/main/java/ru/netology/nmedia/service/FCMService.kt
+++ app/src/main/java/ru/netology/nmedia/service/FCMService.kt
@@ -1,16 +1,90 @@
 package ru.netology.nmedia.service
 
+import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.google.gson.Gson
+import ru.netology.nmedia.R
 
-class FCMService: FirebaseMessagingService() {
+class FCMService : FirebaseMessagingService() {
+
+    private val channelId = "server"
+
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_remote_name)
+            val descriptionText = getString(R.string.channel_remote_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+            val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
 
     override fun onNewToken(token: String) {
-    println(token)
+        println(token)
     }
 
     override fun onMessageReceived(message: RemoteMessage) {
         println(Gson().toJson(message))
+
+        message.data["action"]?.let { actionFromServer ->
+            val action = try {
+                Action.valueOf(actionFromServer)
+            } catch (e: IllegalArgumentException) {
+                return
+            }
+            when (action) {
+                Action.LIKE -> handleLike(
+                    Gson().fromJson(
+                        message.data["content"],
+                        Like::class.java
+                    )
+                )
+            }
+        }
+    }
+
+    fun handleLike(like: Like) {
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentText(
+                getString(
+                    R.string.notification_user_liked,
+                    like.userName,
+                    like.postAuthor
+                )
+            )
+            .build()
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            NotificationManagerCompat.from(this).notify(1, notification)
+        }
     }
 }
+
+enum class Action {
+    LIKE
+}
+
+data class Like(
+    val userId: Int,
+    val userName: String,
+    val postId: Int,
+    val postAuthor: String
+)
